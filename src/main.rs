e09c4ed69a9b761686e85e95f70fe5eb59a7a049
dinhSKM <dinh.le@skymavis.com>
2024-01-01 22:12:31 +0700

    add bind random port

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    println!(
+        "Listening on http://127.0.0.1:{}",
+        listener.local_addr().unwrap().port()
+    );
+    run(listener)?.await
 }
